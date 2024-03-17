26d4decc20f6c67daf005f5903dfa4df35d83189
c1oud-dev <gksmf0187@gmail.com>
2024-03-17 22:31:09 +0900

    [Java] Java basic
    initial commit

---
+++ src/variable/ex/VarEx3.java
@@ -0,0 +1,11 @@
+package variable.ex;
+
+public class VarEx3 {
+    public static void main(String[] args) {
+        long longVar = 10000000000L;
+        System.out.println(longVar);
+        //System.out.println("longVar = " + longVar); //soutv의 결과
+        boolean booleanVar = true;
+        System.out.println(booleanVar);
+    }
+}
